e3f598a0a234db72d3fd3f94a76cc4b40b41b15b
vivekevdt <vivek.evdt@gmail.com>
2025-05-06 06:24:35 +0530

    add cors origin

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use(cors());
 
 // Or, to enable CORS for specific origins:
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL
+  origin: ['http://localhost:5173', 'https://student-nest-web-vivek.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
 }));
