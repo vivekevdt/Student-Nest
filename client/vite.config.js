57ecae56a26ccc092f3a3f84787884f32b5c277f
vivekevdt <vivek.evdt@gmail.com>
2025-05-06 05:33:10 +0530

    add backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      "/api":{
-        target:" http://localhost:3000",
-      
-        secure:false
-
-      }
-    }
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://student-nest-vivek.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
 })
